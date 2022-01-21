493d4baf60816e08421c63743046d915d7ef8786
Mosapi <shahral@mail.ru>
2022-01-21 10:27:42 +0300

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,13 +1,12 @@
 document.onreadystatechange = function(){
 	if(document.readyState === 'complete'){
-		vkl1 = document.querySelector(".bint1"), vkl2 = document.querySelector(".bint2"), dot1 = document.querySelector(".dot1"), dot2 = document.querySelector(".dot2"), setnas = document.querySelector("#setnas");
+		vkl1 = document.querySelector(".bint1"), vkl2 = document.querySelector(".bint2"), vkl3 = document.querySelector(".bint3"), dot1 = document.querySelector(".dot1"), dot2 = document.querySelector(".dot2"), dot3 = document.querySelector(".dot3"), setnas = document.querySelector("#setnas");
 		
 		chrome.storage.sync.get(['setnag'], function(items){
-			if(items == undefined){
+			if(items.setnag == undefined || items == ''){
 				console.log('Настройки пустые');
 			}else{
 				var maner = items.setnag;
-				console.log(maner);
 				for(index = 0; index < maner.length; ++index){
 					gangis = maner[index];
 					var urlx = document.querySelector('input[id="par'+index+'"]');
@@ -39,18 +38,38 @@ document.onreadystatechange = function(){
 		vkl1.addEventListener("click", function(){
 			vkl1.classList.add('active');
 			vkl2.classList.remove('active');
+			vkl3.classList.remove('active');
 			dot1.classList.add('active');
 			dot1.classList.remove('deactive');
 			dot2.classList.add('deactive');
 			dot2.classList.remove('active');
+			dot3.classList.add('deactive');
+			dot3.classList.remove('active');
+			setnas.style.display = 'block';
 		});
 		vkl2.addEventListener("click", function(){
 			vkl2.classList.add('active');
 			vkl1.classList.remove('active');
+			vkl3.classList.remove('active');
 			dot1.classList.add('deactive');
 			dot1.classList.remove('active');
+			dot3.classList.add('deactive');
+			dot3.classList.remove('active');
 			dot2.classList.add('active');
 			dot2.classList.remove('deactive');
+			setnas.style.display = 'block';
+		});
+		vkl3.addEventListener("click", function(){
+			vkl3.classList.add('active');
+			vkl2.classList.remove('active');
+			vkl1.classList.remove('active');
+			dot1.classList.add('deactive');
+			dot1.classList.remove('active');
+			dot2.classList.add('deactive');
+			dot2.classList.remove('active');
+			dot3.classList.add('active');
+			dot3.classList.remove('deactive');
+			setnas.style.display = 'none';
 		});
 	}
 }
